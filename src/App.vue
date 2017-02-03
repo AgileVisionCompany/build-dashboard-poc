e432f99ad3a65eb9b13066615da6eef0d81b1de6
ivictbor <ivictbor@gmail.com>
2017-02-03 15:17:05 +0200

    manual accordion implementation

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
       <md-layout md-gutter class="margin-top-20">
         <md-layout md-align="center">
           <md-card>
-                  <md-column md-align="center">
-                    <md-list class="width-fix" >
-                      <md-list-item disabled>
-                        <a href="#">drie</a>
+            <md-column md-align="center">
+              <md-list class="width-fix">
+                <md-list-item disabled>
+                  <a href="#">drie</a>
                         <span class="fr">
                           <a href="#">docs</a>
                           |
@@ -22,85 +22,104 @@
                             </md-menu-content>
                           </md-menu>
                         </span>
-                      </md-list-item>
-                    </md-list>
+                </md-list-item>
+              </md-list>
 
-                    <md-list class="width-fix">
-                      <md-list-item>
-                        <md-switch class="md-primary" v-model="check1"></md-switch>
-                        <span><a href="#">martin-drie-signup-flow</a></span>
-                        <md-icon></md-icon>
-                      </md-list-item>
-
-                      <md-list-item >
-                        <md-switch class="md-primary" v-model="check2"></md-switch>
-                        <span><a href="#">martin-drie-signup-flow</a></span>
-
-                        <md-list-expand>
-                          <md-list>
-                            <md-list-item class="md-inset">Some content</md-list-item>
-                          </md-list>
-                        </md-list-expand>
-
-                      </md-list-item>
-
-                      <md-list-item class="md-active">
-                        <md-switch class="md-primary" v-model="check3"></md-switch>
-                        <span><a href="#">demo-starter-app</a></span>
-
-                        <md-list-expand>
-                          <md-list>
-                            <md-list-item class="md-inset">
-
-                              <table class="full-width">
-                                <tr>
-                                  <td><a href="#">master</a></td>
-                                  <td><a href="#">visit</a></td>
-                                  <td>2 instances</td>
-                                </tr>
-                                <tr>
-                                  <td><a href="#">todays-news</a></td>
-                                  <td><a href="#">visit</a></td>
-                                  <td>on demand</td>
-                                </tr>
-                                <tr>
-                                  <td><a href="#">feature-make-app-more-simple</a></td>
-                                  <td><a href="#">visit</a></td>
-                                  <td>on demand</td>
-                                </tr>
-                              </table>
-
-                            </md-list-item>
-                          </md-list>
-                        </md-list-expand>
-
-                      </md-list-item>
+              <md-list class="width-fix">
+                <md-list-item @click="listClick(0, $event)">
+                  <md-icon></md-icon>
+                  <span><a href="#">martin-drie-signup-flow</a></span>
+                  <md-switch class="md-primary" v-model="checkState[0]" @change="switchCheck(0)"></md-switch>
+                </md-list-item>
 
 
-                      <md-list-item>
-                        <md-switch class="md-primary" v-model="check4"></md-switch>
-                        <span><a href="#">demo-rails-postgres</a></span>
+                <!-- 1 -->
+                <md-list-item @click="listClick(1, $event)" >
+                  <md-icon v-if="listState[1]">keyboard_arrow_down</md-icon>
+                  <md-icon v-else>keyboard_arrow_right</md-icon>
+                  <span><a href="#">martin-drie-signup-flow</a></span>
+                  <md-switch class="md-primary" v-model="checkState[1]" @change="switchCheck(1)"></md-switch>
 
-                        <md-list-expand>
-                          <md-list>
-                            <md-list-item class="md-inset">Some content</md-list-item>
-                          </md-list>
-                        </md-list-expand>
+                  <span v-show="listState[1]" class="md-divider"></span>
+                </md-list-item>
 
-                      </md-list-item>
+                <md-list-item v-show="listState[1]">
+                  Some content
+                  <span class="md-divider"></span>
+                </md-list-item>
 
-                    </md-list>
+                <!-- 2 -->
+                <md-list-item @click="listClick(2, $event)" >
+                  <md-icon v-if="listState[2]">keyboard_arrow_down</md-icon>
+                  <md-icon v-else>keyboard_arrow_right</md-icon>
+                  <span><a href="#">demo-starter-app</a></span>
+                  <md-switch class="md-primary" v-model="checkState[2]" @change="switchCheck(2)"></md-switch>
 
+                  <span v-show="listState[2]" class="md-divider"></span>
+                </md-list-item>
 
-                  </md-column>
+                <md-list-item v-show="listState[2]">
+                  <table class="full-width">
+                    <tr>
+                      <td><a href="#">master</a></td>
+                      <td><a href="#">visit</a></td>
+                      <td>2 instances</td>
+                    </tr>
+                    <tr>
+                      <td><a href="#">todays-news</a></td>
+                      <td><a href="#">visit</a></td>
+                      <td>on demand</td>
+                    </tr>
+                    <tr>
+                      <td><a href="#">feature-make-app-more-simple</a></td>
+                      <td><a href="#">visit</a></td>
+                      <td>on demand</td>
+                    </tr>
+                  </table>
+                  <span class="md-divider"></span>
+                </md-list-item>
+
+                <!-- 3 -->
+                <md-list-item @click="listClick(3, $event)" >
+                  <md-icon v-if="listState[3]">keyboard_arrow_down</md-icon>
+                  <md-icon v-else>keyboard_arrow_right</md-icon>
+                  <span><a href="#">demo-rails-postgres</a></span>
+                  <md-switch type="submit" class="md-primary" v-model="checkState[3]" @change="switchCheck(3, $event)"></md-switch>
+
+                  <span v-show="listState[3]" class="md-divider"></span>
+                </md-list-item>
+
+                <md-list-item v-show="listState[3]">
+                  Some content
+                  <span class="md-divider"></span>
+                </md-list-item>
+
+              </md-list>
+
+            </md-column>
           </md-card>
         </md-layout>
       </md-layout>
+
+      <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog1">
+        <md-dialog-title>Are you sure</md-dialog-title>
+
+        <md-dialog-content>Are you sure?</md-dialog-content>
+
+        <md-dialog-actions>
+          <md-button class="md-primary" @click="closeDialog('dialog1')">Cancel</md-button>
+          <md-button class="md-primary" @click="closeDialog('dialog1')">Ok</md-button>
+        </md-dialog-actions>
+      </md-dialog>
     </div>
+
+
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+
   export default {
     name: 'app',
     data () {
@@ -108,7 +127,32 @@
         check1: false,
         check2: true,
         check3: true,
-        check4: false
+        check4: false,
+        listState: [false, true, true, false],
+        checkState: [false, true, true, false]
+      }
+    },
+    watch: {
+    },
+    methods: {
+      openDialog (ref) {
+        this.$refs[ref].open()
+      },
+      closeDialog (ref) {
+        this.$refs[ref].close()
+      },
+      listClick (i, event) {
+        if (event.toElement.className !== 'md-switch-holder') {
+          Vue.set(this.listState, i, !this.listState[i])
+        }
+      },
+      switchCheck (i, event) {
+        console.log(event)
+       // event.preventDefault()
+//        console.log(this.checkState[i])
+//        Vue.set(this.checkState, i, false)
+//        console.log(this.checkState[i])
+        this.openDialog('dialog1')
       }
     }
 
@@ -119,16 +163,19 @@
   body {
     background-color: #e1e4e8 !important;
   }
-  .margin-top-20{
+
+  .margin-top-20 {
     margin-top: 20px;
   }
+
   .width-fix {
     width: 700px
   }
 
   .full-width {
-    width:100%;
+    width: 100%;
   }
+
   .fr {
     float: right;
   }
