8114e385d7d204cb5514b6703642d122bec78e30
ivictbor <ivictbor@gmail.com>
2017-02-03 13:56:59 +0200

    a little bit darker background, md-card

--- src/App.vue
+++ src/App.vue
@@ -3,94 +3,97 @@
     <div>
       <md-layout md-gutter class="margin-top-20">
         <md-layout md-align="center">
-          <md-column md-align="center">
-            <md-layout class="width-fix inline">
-              <a href="#">drie</a>
-              <span class="fr">
-                <a href="#">docs</a>
-                |
-                <a href="#">support</a>
-                |
-                <md-menu>
-                  <a href="#" md-menu-trigger>ben@ACME</a>
+          <md-card>
+                  <md-column md-align="center">
+                    <md-list class="width-fix" >
+                      <md-list-item disabled>
+                        <a href="#">drie</a>
+                        <span class="fr">
+                          <a href="#">docs</a>
+                          |
+                          <a href="#">support</a>
+                          |
+                          <md-menu>
+                            <a href="#" md-menu-trigger>ben@ACME</a>
 
-                  <md-menu-content>
-                    <md-menu-item>Item 1</md-menu-item>
-                    <md-menu-item>Logout</md-menu-item>
-                  </md-menu-content>
-                </md-menu>
-              </span>
-            </md-layout>
+                            <md-menu-content>
+                              <md-menu-item>Item 1</md-menu-item>
+                              <md-menu-item>Logout</md-menu-item>
+                            </md-menu-content>
+                          </md-menu>
+                        </span>
+                      </md-list-item>
+                    </md-list>
 
-            <md-list class="width-fix">
-              <md-list-item>
-                <md-switch class="md-primary" v-model="check1"></md-switch>
-                <span><a href="#">martin-drie-signup-flow</a></span>
-                <md-icon></md-icon>
-              </md-list-item>
+                    <md-list class="width-fix">
+                      <md-list-item>
+                        <md-switch class="md-primary" v-model="check1"></md-switch>
+                        <span><a href="#">martin-drie-signup-flow</a></span>
+                        <md-icon></md-icon>
+                      </md-list-item>
 
-              <md-list-item >
-                <md-switch class="md-primary" v-model="check2"></md-switch>
-                <span><a href="#">martin-drie-signup-flow</a></span>
+                      <md-list-item >
+                        <md-switch class="md-primary" v-model="check2"></md-switch>
+                        <span><a href="#">martin-drie-signup-flow</a></span>
 
-                <md-list-expand>
-                  <md-list>
-                    <md-list-item class="md-inset">Some content</md-list-item>
-                  </md-list>
-                </md-list-expand>
+                        <md-list-expand>
+                          <md-list>
+                            <md-list-item class="md-inset">Some content</md-list-item>
+                          </md-list>
+                        </md-list-expand>
 
-              </md-list-item>
+                      </md-list-item>
 
-              <md-list-item class="md-active">
-                <md-switch class="md-primary" v-model="check3"></md-switch>
-                <span><a href="#">demo-starter-app</a></span>
+                      <md-list-item class="md-active">
+                        <md-switch class="md-primary" v-model="check3"></md-switch>
+                        <span><a href="#">demo-starter-app</a></span>
 
-                <md-list-expand>
-                  <md-list>
-                    <md-list-item class="md-inset">
+                        <md-list-expand>
+                          <md-list>
+                            <md-list-item class="md-inset">
 
-                      <table class="full-width">
-                        <tr>
-                          <td><a href="#">master</a></td>
-                          <td><a href="#">visit</a></td>
-                          <td>2 instances</td>
-                        </tr>
-                        <tr>
-                          <td><a href="#">todays-news</a></td>
-                          <td><a href="#">visit</a></td>
-                          <td>on demand</td>
-                        </tr>
-                        <tr>
-                          <td><a href="#">feature-make-app-more-simple</a></td>
-                          <td><a href="#">visit</a></td>
-                          <td>on demand</td>
-                        </tr>
-                      </table>
+                              <table class="full-width">
+                                <tr>
+                                  <td><a href="#">master</a></td>
+                                  <td><a href="#">visit</a></td>
+                                  <td>2 instances</td>
+                                </tr>
+                                <tr>
+                                  <td><a href="#">todays-news</a></td>
+                                  <td><a href="#">visit</a></td>
+                                  <td>on demand</td>
+                                </tr>
+                                <tr>
+                                  <td><a href="#">feature-make-app-more-simple</a></td>
+                                  <td><a href="#">visit</a></td>
+                                  <td>on demand</td>
+                                </tr>
+                              </table>
 
-                    </md-list-item>
-                  </md-list>
-                </md-list-expand>
+                            </md-list-item>
+                          </md-list>
+                        </md-list-expand>
 
-              </md-list-item>
-
-
-              <md-list-item>
-                <md-switch class="md-primary" v-model="check4"></md-switch>
-                <span><a href="#">demo-rails-postgres</a></span>
-
-                <md-list-expand>
-                  <md-list>
-                    <md-list-item class="md-inset">Some content</md-list-item>
-                  </md-list>
-                </md-list-expand>
-
-              </md-list-item>
-
-            </md-list>
+                      </md-list-item>
 
 
-          </md-column>
+                      <md-list-item>
+                        <md-switch class="md-primary" v-model="check4"></md-switch>
+                        <span><a href="#">demo-rails-postgres</a></span>
 
+                        <md-list-expand>
+                          <md-list>
+                            <md-list-item class="md-inset">Some content</md-list-item>
+                          </md-list>
+                        </md-list-expand>
+
+                      </md-list-item>
+
+                    </md-list>
+
+
+                  </md-column>
+          </md-card>
         </md-layout>
       </md-layout>
     </div>
@@ -113,6 +116,9 @@
 </script>
 
 <style>
+  body {
+    background-color: #e1e4e8 !important;
+  }
   .margin-top-20{
     margin-top: 20px;
   }
